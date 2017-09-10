aafd628a5c516bfe3f4cd88ee5cdeb54024bffc2
Murat Tunaboylu <murat@antiverse.io>
2017-09-10 17:38:18 +0200

    Prediction input file preprocessor New input file for batch prediction

---
+++ trainer/prediction.preprocessor.py
@@ -0,0 +1,36 @@
+def is_int(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+
+reader = open('../data/test.csv')
+writer = open('../data/test.json', 'w')
+
+header = reader.readline()
+line = reader.readline()
+
+while line:
+    col = line.rstrip().split(',')
+    pclass = col[1]
+    sex = col[4]
+    age = col[5] if col[5] != "" else 0
+    sibsp = col[6]
+    parch = col[7]
+    fare = col[9]
+    cabin_class = col[10][:1]
+    cabin_nos = col[10][1:]
+    cabin_no = cabin_nos.split(' ')[0]
+    cabin_no = cabin_no if is_int(cabin_no) else 0
+    embarked = col[11]
+
+    json = '{{"Pclass":{},"Sex":"{}", "Age":{}, "SibSp":{}, "Parch":{}, "Fare":{}, "Embarked":"{}", "CabinClass":"{}"' \
+           ', "CabinNo":{}}}\n'.format(pclass, sex, age, sibsp, parch, fare, embarked, cabin_class, cabin_no)
+    writer.write(json)
+
+    line = reader.readline()
+
+reader.close()
+writer.close()
